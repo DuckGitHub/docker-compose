cd19a7fcfd39f6f5713ba7d6d00a75ece87b51c2
Pato <you@example.com>
2021-03-30 23:54:35 -0300

    3er commit

--- src/database.js
+++ src/database.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://mongo/mydatabase")
+  .connect("mongodb://mongo/mydatabase", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then((db) => console.log("db is connected"))
   .catch((err) => console.log("err: ", err));
